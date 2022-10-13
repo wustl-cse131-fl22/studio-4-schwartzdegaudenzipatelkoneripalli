4d318da283c4268bc12710955922288ddfe13829
DovSchwartz <schwartzdov75@gmai.com>
2022-10-13 13:43:21 -0500

    s4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.filledRectangle(.5,.5,.5,.4);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.5, .4, .3);
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.filledRectangle(0.5, 0.5, .4, .1);
+		StdDraw.setPenColor(Color.GRAY);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		StdDraw.filledCircle(0.25, 0.5, 0.1);
+		StdDraw.filledCircle(0.75, 0.5, 0.1);
 	}
 }
